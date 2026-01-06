4a06d990eace8875ce79909374f708265fb31d98
Benfares-med <meedbenfares123@gmail.com>
2026-01-06 09:22:59 +0100

    Update JacksonConverterService.java

--- src/main/java/org/example/xmljsonconverter/JacksonConverterService.java
+++ src/main/java/org/example/xmljsonconverter/JacksonConverterService.java
@@ -12,9 +12,7 @@ public class JacksonConverterService {
     private final XmlMapper xmlMapper;
 
     public JacksonConverterService() {
-        // Configuration spécifique pour XML
         XMLInputFactory xmlInputFactory = XMLInputFactory.newFactory();
-        // Permet de gérer les namespaces sans casser la structure JSON
         xmlInputFactory.setProperty(XMLInputFactory.IS_NAMESPACE_AWARE, false);
 
         xmlMapper = new XmlMapper(xmlInputFactory);
@@ -22,7 +20,6 @@ public class JacksonConverterService {
 
     public String xmlToJsonAuto(String xml) {
         try {
-            // Jackson gère nativement le CDATA en extrayant le texte contenu dedans
             JsonNode node = xmlMapper.readTree(xml.getBytes());
             return jsonMapper.writerWithDefaultPrettyPrinter().writeValueAsString(node);
         } catch (Exception e) {
@@ -33,7 +30,6 @@ public class JacksonConverterService {
     public String jsonToXmlAuto(String json) {
         try {
             JsonNode node = jsonMapper.readTree(json);
-            // withRootName "root" assure qu'on a toujours une racine XML valide
             return xmlMapper.writerWithDefaultPrettyPrinter().withRootName("root").writeValueAsString(node);
         } catch (Exception e) {
             return "ERREUR JACKSON (JSON -> XML) : \n" + e.getMessage();
